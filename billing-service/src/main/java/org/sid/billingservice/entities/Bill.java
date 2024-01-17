cc178bc7ad3b5a1e4a6221eee14407a00f02770f
Yasserkb <koubachi.yasser@gmail.com>
2024-01-17 15:44:42 +0100

    full project

---
+++ billing-service/src/main/java/org/sid/billingservice/entities/Bill.java
@@ -0,0 +1,22 @@
+package org.sid.billingservice.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.sid.billingservice.model.Customer;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+@Entity @AllArgsConstructor @NoArgsConstructor @Data @Builder
+public class Bill {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date billDate;
+    private Long customerId;
+    @OneToMany(mappedBy = "bill")
+    private List<ProductItem> productItems;
+    @Transient
+    private Customer customer;
+}
